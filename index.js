9b163c904c18fdb89905a994139f1754bd0d1e1a
cofcat456 <a25690756@gmail.com>
2024-10-29 00:31:08 +0800

    feat: poc working

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 // ==UserScript==
 // @name         Open in New Tab
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.0
 // @description  Always open a new tab
-// @author       You
+// @author       JackCodeTW
+// @license      MIT
 // @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
+
+(function () {
+  'use strict'
+
+  function externalize() {
+    document.querySelectorAll('a')
+      .forEach((r) => {
+        if (!r.hasAttribute('target')) {
+          r.setAttribute('target', '_blank')
+        }
+      })
+  }
+
+  ////////////////////////////////////////
+
+  function run() {
+    externalize()
+  }
+
+  run()
+})()
